4fda7e379a8b2928d5d0d758cc99f4dbf4e27690
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2022-04-24 20:13:34 -0300

    [feature-frontend-04] added a not found page

--- src/features/not_found/index.js
+++ src/features/not_found/index.js
@@ -1,5 +1,16 @@
-function NotFound() {
-  return <h1>NotFound</h1>;
-}
+import doctor from '../../assets/images/not_found_doctor.svg';
+import ellipse from '../../assets/images/ellipse.svg';
+import './style.css'
 
-export default NotFound;
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className='error'>ERRO!</h1>
+      <div className='test'>
+        <p className='text'>ah, não! parece que o doutor não conseguiu resolver seu problema!</p>
+      </div>
+      <img className='doctor' src={doctor} alt=""/>
+      <img className='ellipse' src={ellipse} alt=""/>
+    </div>
+  )
+}
