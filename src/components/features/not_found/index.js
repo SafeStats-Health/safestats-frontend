83601f4552f4adeea7d92b55b2c278dc7b5f96ec
Pedro Henrique Moreira <63320967+PedroMoreira87@users.noreply.github.com>
2022-04-29 23:22:55 -0300

    Feature frontend 04 (#3)
    * [feature-frontend-04] added a not found page
    
    * [feature-frontend-04] Css improvements
    
    * [feature-frontend-04] Improvements

---
+++ src/components/features/not_found/index.js
@@ -0,0 +1,21 @@
+import doctor from '../../../assets/images/not_found_doctor.svg';
+import ellipse from '../../../assets/images/ellipse.svg';
+import t from '../../../i18n/translate';
+import './style.css'
+
+export default function NotFound() {
+  return (
+    <div>
+      <span className='error'>{t('ERROR')}</span>
+      <div>
+        <span className='text'>{t('ERROR_MESSAGE')}</span>
+      </div>
+      <div className='img-pos-row'>
+        <div className='img-pos-col'>
+          <img className='doctor' src={doctor} alt=""/>
+          <img className='ellipse' src={ellipse} alt=""/>
+        </div>
+      </div>
+    </div>
+  )
+}
