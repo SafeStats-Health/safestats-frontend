1b916c31cc515c4cf2684992ec2809240d0e85b8
Pedro Henrique Moreira <63320967+PedroMoreira87@users.noreply.github.com>
2022-04-29 23:22:55 -0300

    Feature frontend 04 (#3)
    * [feature-frontend-04] added a not found page
    
    * [feature-frontend-04] Css improvements
    
    * [feature-frontend-04] Improvements

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from '../pages/home';
-import Login from '../pages/login';
-import Register from '../pages/register';
-import NotFound from '../pages/not_found';
+import Home from '../features/home';
+import Login from '../features/login';
+import Register from '../features/register';
+import NotFound from '../components/features/not_found';
+import EmailConfirm from '../components/features/email_confirm';
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
         <Route index element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/email_confirm' element={<EmailConfirm />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
