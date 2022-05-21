379a63099afeec25cffab7217e71d2b6ad50d973
unknown <bruno.bergamini@outlook.com>
2022-05-20 22:01:49 -0300

    [feature-frontend-05] updating branch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,7 @@
-import styles from './styles.module.css';
-import Map from '../../components/core/c_map';
-
 function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Map />
     </div>
   );
 }
