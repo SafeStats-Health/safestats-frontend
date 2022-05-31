65404de880e98e94361706fe3cb4f34d261e6f33
David SdS <dave.sathler@gmail.com>
2022-05-30 23:52:46 -0300

    [feature-frontend-14] button links working

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,47 +1,38 @@
-import { Link } from 'react-router-dom';
 import Case from '../../assets/images/home/case.png';
 import DoctorStethoscope from '../../assets/images/home/doctor_stethoscope.png';
 import Hologram from '../../assets/images/home/hologram.png';
 import Microscope from '../../assets/images/home/microscope.png';
 import MicroscopeCloseup from '../../assets/images/home/microscope_closeup.png';
 import Ome from '../../assets/images/home/ome.png';
-import SafeStats from '../../assets/images/home/safe_stats_alt.png';
 import Stethoscope from '../../assets/images/home/stethoscope.png';
-import User from '../../assets/images/home/user.png';
 import t from '../../i18n/translate';
 import styles from './styles.module.css';
-import CButton from '../../components/core/c_button'
+import CButton from '../../components/core/c_button';
+import Header from '../../components/header';
+import { Link } from 'react-router-dom';
 
 function Home() {
+
+  function goToRegister() {
+    document.getElementById('registerLink').click();
+  }
+
   return (
     <div className={styles.home}>
+      <Link to='/register' id='registerLink' />
       <div className={styles['top-section']}>
         <div className={styles.ellipse} />
         <div className={styles['home-container']}>
-          <div className={styles.header}>
-            <img className={styles['img-safe-stats']} src={SafeStats} />
-            <div className={styles.links}>
-              <Link to='/login' className=''>
-                {t('ABOUT_US')}
-              </Link>
-              <Link to='/login' className=''>
-                {t('CONTACT')}
-              </Link>
-              <Link to='/login' className=''>
-                {t('REGISTER')}
-              </Link>
-              <img className={styles['img-user']} src={User} />
-            </div>
-          </div>
+          <Header alt />
           <div className={styles['main-info']}>
             <div className={styles['main-text']}>
               <span className={styles['main-title']}>{t('TECH_FOR_THE_HEALTH')}</span>
               <div>
-                <span className={styles['main-paragraph']}>{t('DO_YOU_WANT_SAFESTATS')}</span>
+                <span className={styles['main-paragraph']}>{t('DO_YOU_WANT_SAFESTATS')}</span> <br/>
                 <span className={styles['main-paragraph']}>{t('REGISTER_ITS_FREE')}</span>
               </div>
               <div className={styles['button']} style={{minWidth: '50%'}}>
-                <CButton label={t('REGISTER_YOURSELF')} backgroundColor="#003A95" />
+                <CButton label={t('REGISTER_YOURSELF')} backgroundColor="#003A95" onClick={goToRegister} />
               </div>
             </div>
             <div className={styles['main-image']}>
@@ -73,7 +64,7 @@ function Home() {
               src={DoctorStethoscope}
             />
           </div>
-          <div className={styles['button']}><CButton label={t('REGISTER_YOURSELF')} backgroundColor="#003A95" /></div>
+          <div className={styles['button']}><CButton label={t('REGISTER_YOURSELF')} backgroundColor="#003A95" onClick={goToRegister} /></div>
           <div className={styles['card-group']}>
             <div className={`${styles['card']} ${styles['card-blue']}`}>
               <img className={styles['card-image']} src={Microscope} />
@@ -88,7 +79,7 @@ function Home() {
               <span className={styles['card-text']}>{t('TEXT_STETHOSCOPE')}</span>
             </div>
           </div>
-          <div className={styles['button']}><CButton label={t('REGISTER_YOURSELF')} backgroundColor="#003A95" /></div>
+          <div className={styles['button']}><CButton label={t('REGISTER_YOURSELF')} backgroundColor="#003A95" onClick={goToRegister} /></div>
         </div>
       </div>
     </div>
