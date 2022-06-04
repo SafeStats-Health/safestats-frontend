6da9da92949bd92646dff3bfef11ba394b76b745
unknown <bruno.bergamini@outlook.com>
2022-06-03 21:43:40 -0300

    [feature-frontend-06] fix email_confirm

--- src/pages/email_confirm/index.js
+++ src/pages/email_confirm/index.js
@@ -1,20 +1,32 @@
-import Button from '@mui/material/Button';
-import styles from './styles.module.css';
-import t from '../../i18n/translate';
+import { Link } from 'react-router-dom';
 import doctor from '../../assets/images/email_confirm_doctor.png';
 import safeStats from '../../assets/images/safe_stats.svg';
+import CButton from '../../components/core/c_button';
+import t from '../../i18n/translate';
+import styles from './styles.module.css';
 
 export default function EmailConfirm() {
+  function goToLogin() {
+    document.getElementById('loginLink').click();
+  }
+
   return (
-    <div className={styles.itemsPos}>
-      <img src={safeStats} alt="Safe Stats" className={styles.safeStats}/>
-      <div className={styles.textPos}>
-        <span className={styles.hello}>{t('HELLO')}</span>
-        <span className={styles.email}>{t('EMAIL_MESSAGE')}</span>
-        <Button variant="contained" className={styles.button}>{t('BACK_TO_LOGIN')}</Button>
+    <div className={styles['email-confirm']}>
+      <Link to='/login' id='loginLink' />
+      <img src={safeStats} alt='Safe Stats' className={styles.logo} />
+      <div className={styles.text}>
+        <div className={styles['text-container']}>
+          <span className={styles.title}>{t('HELLO')}</span>
+          <span className={styles.subtitle}>{t('EMAIL_MESSAGE')}</span>
+          <CButton
+            label={t('BACK_TO_LOGIN')}
+            buttonContainer={styles['button-container']}
+            onClick={goToLogin}
+          />
+        </div>
       </div>
-      <div className={styles.doctorPos}>
-        <img src={doctor} alt="Doctor" className={styles.doctor}/>
+      <div>
+        <img src={doctor} alt='Doctor' className={styles.doctor} />
       </div>
     </div>
   );
