3440a906b75e4482a96710e95a7c943e297b8db2
Pedro Henrique Moreira <63320967+PedroMoreira87@users.noreply.github.com>
2022-06-14 12:58:40 -0300

    Feature frontend 16 route protection (#16)
    * feature-frontend-16-route-protection: created protected.route
    
    * feature-frontend-16-route-protection: small adjust about the router
    
    * feature-frontend-16-route-protection: small adjust
    
    * feature-frontend-16-route-protection: added route protection
    
    * feature-frontend-16-route-protection: removed fragment from routes
    
    * feature-frontend-16-route-protection: route protection done

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AboutUs from '../pages/about_us';
-import Contact from '../pages/contact';
+import {Route, Routes} from 'react-router-dom';
+import {ProtectedRoute} from "./protected.route";
+
 import EmailConfirm from '../pages/email_confirm';
 import Home from '../pages/home';
 import Login from '../pages/login';
 import Map from '../pages/map';
 import NotFound from '../pages/not_found';
 import Register from '../pages/register';
+import AboutUs from '../pages/about_us';
+import Contact from '../pages/contact';
 import ResetPassword from '../pages/reset_password';
 import UserProfile from '../pages/user_profile';
 
@@ -15,46 +17,18 @@ const Router = () => {
   return (
     <div>
       <Routes>
-        <Route
-          index
-          element={<Home/>}
-        />
-        <Route
-          path='/login'
-          element={<Login/>}
-        />
-        <Route
-          path='/register'
-          element={<Register/>}
-        />
-        <Route
-          path='/email_confirm'
-          element={<EmailConfirm/>}
-        />
-        <Route
-          path='/user_profile'
-          element={<UserProfile/>}
-        />
-        <Route
-          path='/map'
-          element={<Map/>}
-        />
-        <Route
-          path='/about_us'
-          element={<AboutUs/>}
-        />
-        <Route
-          path='/contact'
-          element={<Contact/>}
-        />
-        <Route
-          path='/reset_password'
-          element={<ResetPassword />}
-        />
-        <Route
-          path='*'
-          element={<NotFound/>}
-        />
+        <Route index element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route exact path='/register' element={<Register/>}/>
+        <Route exact path='/user_profile' element={<ProtectedRoute/>}>
+          <Route path='/user_profile' element={<UserProfile/>}/>
+        </Route>
+        <Route path='/email_confirm' element={<EmailConfirm/>}/>
+        <Route path='/map' element={<Map/>}/>
+        <Route path='/about_us' element={<AboutUs/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/reset_password' element={<ResetPassword/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
   );
