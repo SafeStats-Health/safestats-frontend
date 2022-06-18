998cfe3444a28f08614533bf13b8f73127d2af84
Kamylle Wilgozz <kamyllewilgozzd@gmail.com>
2022-06-17 23:47:54 -0300

    criação tela de contato

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.css';
 import Header from '../../components/header';
 import t from '../../i18n/translate';
+import imgContato from '../../assets/images/contact/mulherContato.svg';
 
 function Home() {
   return (
@@ -8,7 +9,28 @@ function Home() {
       <div className={styles['top-section']}>
         <div className={styles['home-container']}>
           <Header />
-          {t('CONTACT')}
+
+          <div className={styles['contact-container']}>
+
+            <div>
+              <h1 className={styles['tituloContato']}>{t('CONTACT_TITLE')}</h1>
+              <p className={styles['fraseContato']}>{t('CONTACT_SUBTITLE')}</p>
+            </div>
+
+            <div>
+              <img className={styles['mocaContato']} src={imgContato} />
+            </div>
+          </div>
+
+          <div>
+            <h5 className={styles['dadosContatoTitulo']}> {t('CONTACT_PHONE_TITLE')}</h5>
+            <p className={styles['dadosContatoSubtitulo']}>{t('CONTACT_PHONE_NUMBER')}</p> 
+          </div>
+
+          <div>
+            <h5 className={styles['dadosContatoTitulo']}>{t('CONTACT_SOCIALMEDIA_TITLE')}</h5>
+            <p className={styles['dadosContatoSubtitulo']}>{t('CONTACT_SOCIALMEDIA_SUBTITLE')}</p>
+          </div>
         </div>
       </div>
     </div>
