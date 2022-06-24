fe419ea9b2a868a955123687232bf3385ecf5969
Pedro Henrique Moreira <63320967+PedroMoreira87@users.noreply.github.com>
2022-06-23 21:38:24 -0300

    fix-user-data: updated general page (#23)
    * fix-user-data: updated general page
    
    * fix-user-data: merge master

--- src/pages/user_profile/general/index.jsx
+++ src/pages/user_profile/general/index.jsx
@@ -1,26 +1,28 @@
 import styles from './styles.module.css';
 import t from '../../../i18n/translate';
 import dummyImg from '../../../assets/images/dummy_img.png';
-import {useEffect, useState} from "react";
 
-function General() {
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    return () => {
-      setUser(JSON.parse(localStorage.getItem('login')).user);
-    }
-  }, []);
+export default function General() {
+
+  const usedData = JSON.parse(localStorage.getItem('login'));
+  let userPlan, userBirthdate, userPhone, userDonatedBlood, userBloodType, userLegalRepresentative;
+
+  usedData.user.healthPlan ? userPlan = usedData.user.healthPlan + ' - ' : userPlan = ''
+  usedData.user.birthdate ? userBirthdate = usedData.user.birthdate + ' anos - ' : userBirthdate = ' '
+  usedData.user.phone ? userPhone = usedData.user.phone + ' - ' : userPhone = ' '
+  usedData.user.didDonateBlood ? userDonatedBlood = usedData.user.didDonateBlood + ', ' : userDonatedBlood = ' '
+  usedData.user.bloodType ? userBloodType = usedData.user.bloodType + ' - ' : userBloodType = ' '
+  usedData.user.legalRepresentative ? userLegalRepresentative = usedData.user.legalRepresentative : userLegalRepresentative = ''
 
   return (
     <div className={styles.general}>
       <div className={styles['user-data']}>
         <img src={dummyImg} alt="Dummy Img" className={styles['user-data-pic']}/>
-        <span className={styles['user-data-name']}>{user.name}</span>
+        <span className={styles['user-data-name']}>{usedData.user.name}</span>
         <span className={styles['user-data-title']}>{t('SUMMARY_TITLE')}</span>
-        <span className={styles['user-data-info']}>plano de saude unimed básico enfermaria - 22 anos, natural de curitiba - gênero feminino - doa sangue, tipo A+ - responsável legal marcos lemos</span>
+        <span
+          className={styles['user-data-info']}>{`${userPlan+userBirthdate+userPhone+userDonatedBlood+userBloodType+userLegalRepresentative}`}</span>
       </div>
     </div>
   );
 }
-
-export default General;
